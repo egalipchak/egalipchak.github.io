22fd321a61ab8d4c9358a34831711da4171e6eec
Eugene Galipchak <ygalipchak@gmail.com>
2023-07-26 21:14:25 -0700

    pushing images

--- wwkd/src/App.js
+++ wwkd/src/App.js
@@ -9,21 +9,21 @@ import KanoMatchesPage from './components/KanoMatchesPage';
 
 const App = () => {
   const recentImages = [
-    { src: require('./assets/images/mediumPuzzles/WWKDGoldfish1.png'), description: 'Recent Puzzle 1' },
+    { src: require('./assets/images/mediumPuzzles/WWKDGoldfish1.jpg'), description: 'Recent Puzzle 1' },
   ];
   const easyImages = [
-    { src: require('./assets/images/easyPuzzles/WWKD12.png'), description: 'Easy Puzzle 1' },
+    { src: require('./assets/images/easyPuzzles/WWKD12.jpg'), description: 'Easy Puzzle 1' },
   ];
   const mediumImages = [
-    { src: require('./assets/images/mediumPuzzles/WWKD4.png'), description: 'Medium Puzzle 1' },
-    { src: require('./assets/images/mediumPuzzles/WWKD6.png'), description: 'Medium Puzzle 2' },
-    { src: require('./assets/images/mediumPuzzles/WWKD7.png'), description: 'Medium Puzzle 2' },
-    { src: require('./assets/images/mediumPuzzles/WWKDGoldfish1.png'), description: 'Medium Puzzle 2' },
+    { src: require('./assets/images/mediumPuzzles/WWKD4.jpg'), description: 'Medium Puzzle 1' },
+    { src: require('./assets/images/mediumPuzzles/WWKD6.jpg'), description: 'Medium Puzzle 2' },
+    { src: require('./assets/images/mediumPuzzles/WWKD7.jpg'), description: 'Medium Puzzle 2' },
+    { src: require('./assets/images/mediumPuzzles/WWKDGoldfish1.jpg'), description: 'Medium Puzzle 2' },
   ];
   const hardImages = [
-    { src: require('./assets/images/hardPuzzles/WWKD8.png'), description: 'Hard Puzzle 1' },
-    { src: require('./assets/images/hardPuzzles/WWKD9.png'), description: 'Hard Puzzle 2' },
-    { src: require('./assets/images/hardPuzzles/WWKD11.png'), description: 'Medium Puzzle 2' },
+    { src: require('./assets/images/hardPuzzles/WWKD8.jpg'), description: 'Hard Puzzle 1' },
+    { src: require('./assets/images/hardPuzzles/WWKD9.jpg'), description: 'Hard Puzzle 2' },
+    { src: require('./assets/images/hardPuzzles/WWKD11.jpg'), description: 'Medium Puzzle 2' },
   ];
 
   return (
--- wwkd/src/components/GuidesPage.js
+++ wwkd/src/components/GuidesPage.js
@@ -3,7 +3,7 @@ import React from 'react';
 const GuidesPage = () => {
   return (
     <div className="page">
-      <h2 style={{ color: '#fff' }}>   TBD... should feature Kano guide to beginner and intermediate players</h2>
+      <h2 style={{ color: '#fff' }}>   TBD... should feature Kano guide to beginner and intermmediate players</h2>
     </div>
   );
 };
