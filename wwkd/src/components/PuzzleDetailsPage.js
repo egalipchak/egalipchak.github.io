6f306fd4342e3e73dae10bad5236dbfb06b93a71
Eugene Galipchak <ygalipchak@gmail.com>
2023-08-01 16:05:36 -0700

    bugged Loading animation

--- wwkd/src/components/PuzzleDetailsPage.js
+++ wwkd/src/components/PuzzleDetailsPage.js
@@ -49,49 +49,51 @@ const PuzzleDetailsPage = ({ images }) => {
   const ogImageUrl = puzzle.src;
 
   return (
-    <div className={`page-wrapper ${imageLoaded ? 'show' : 'hide'}`}>
-      {isLoading && <div className="loading">{loadingText}</div>} {/* Show loading indicator while image is loading*/}
+    <>
       <MenuBar />
-      <Helmet>
-        <title>{puzzle.title}</title>
-        <meta property="og:title" content={puzzle.title} />
-        <meta property="og:description" content={`Difficulty: ${puzzle.difficulty}`} />
-        <meta property="og:image" content={ogImageUrl} />
-        <meta property="og:url" content={`https://www.whatwouldkanodo.com/#/puzzles/${puzzle.id}`} />
+      {isLoading && <div className="loading">{loadingText}</div>} {/* Show loading indicator while image is loading*/}
+      <div className={`page-wrapper ${imageLoaded ? 'show' : 'hide'}`}>
+        <Helmet>
+          <title>{puzzle.title}</title>
+          <meta property="og:title" content={puzzle.title} />
+          <meta property="og:description" content={`Difficulty: ${puzzle.difficulty}`} />
+          <meta property="og:image" content={ogImageUrl} />
+          <meta property="og:url" content={`https://www.whatwouldkanodo.com/#/puzzles/${puzzle.id}`} />
 
-        {/* Twitter-specific meta tags */}
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content={puzzle.title} />
-        <meta name="twitter:description" content={`Difficulty: ${puzzle.difficulty}`} />
-        <meta name="twitter:image" content={ogImageUrl} />
+          {/* Twitter-specific meta tags */}
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:title" content={puzzle.title} />
+          <meta name="twitter:description" content={`Difficulty: ${puzzle.difficulty}`} />
+          <meta name="twitter:image" content={ogImageUrl} />
 
-        {/* Discord-specific meta tags */}
-        <meta property="og:site_name" content="Your Website Name" />
-        <meta property="og:type" content="website" />
+          {/* Discord-specific meta tags */}
+          <meta property="og:site_name" content="Your Website Name" />
+          <meta property="og:type" content="website" />
 
-        {/* General SEO meta tags */}
-        <meta name="description" content={`Difficulty: ${puzzle.difficulty} - ${puzzle.description}`} />
-        <meta name="image" content={ogImageUrl} />
-        <link rel="canonical" href={`https://www.whatwouldkanodo.com/#/puzzles/${puzzle.id}`} />
-      </Helmet>
-      <div className="content-container">
-        {/* The "show" and "hide" classes will control the visibility of the content */}
-        <div className="puzzle-details-page">
-          <h2>{puzzle.title}</h2>
-          <img src={puzzle.src} alt={`Puzzle`} onLoad={handleImageLoad} />
-          <p className="text-outline">Difficulty: {puzzle.difficulty}</p>
-          <p className="text-outline">Description: {puzzle.description}</p>
-          <div className="solution-link-container">
-            {puzzle.url !== "" && (
-              <a href={puzzle.url} target="_blank" rel="noopener noreferrer" className="solution-link">
-                ~~Solution Discussion Link~~
-              </a>
-            )}
+          {/* General SEO meta tags */}
+          <meta name="description" content={`Difficulty: ${puzzle.difficulty} - ${puzzle.description}`} />
+          <meta name="image" content={ogImageUrl} />
+          <link rel="canonical" href={`https://www.whatwouldkanodo.com/#/puzzles/${puzzle.id}`} />
+        </Helmet>
+        <div className="content-container">
+          {/* The "show" and "hide" classes will control the visibility of the content */}
+          <div className="puzzle-details-page">
+            <h2>{puzzle.title}</h2>
+            <img src={puzzle.src} alt={`Puzzle`} onLoad={handleImageLoad} />
+            <p className="text-outline">Difficulty: {puzzle.difficulty}</p>
+            <p className="text-outline">Description: {puzzle.description}</p>
+            <div className="solution-link-container">
+              {puzzle.url !== "" && (
+                <a href={puzzle.url} target="_blank" rel="noopener noreferrer" className="solution-link">
+                  ~~Solution Discussion Link~~
+                </a>
+              )}
+            </div>
           </div>
         </div>
+        <BottomBar />
       </div>
-      <BottomBar />
-    </div>
+    </>
   );
 };
 
