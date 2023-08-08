1b7f2181f5b1514f44d1addd8760bb8d007081bd
Eugene Galipchak <ygalipchak@gmail.com>
2023-08-08 01:15:19 -0700

    added gentle glow to puzzles and loading bar for puzzlepage

--- wwkd/src/components/PuzzlePage.js
+++ wwkd/src/components/PuzzlePage.js
@@ -1,15 +1,16 @@
-/* PuzzlePage.js */
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import MenuBar from './MenuBar';
 import BottomBar from './BottomBar';
 import SortDropdown from './SortDropdown';
+import { BarLoader } from 'react-spinners';
 import './PuzzlePage.css';
 
 const PuzzlePage = ({ images }) => {
   const navigate = useNavigate();
   const [sortBy, setSortBy] = useState('date');
   const [sortOrder, setSortOrder] = useState('desc');
+  const [allImagesLoaded, setAllImagesLoaded] = useState(false);
 
   const redirectToPuzzleDetails = (puzzle) => {
     navigate(`/puzzles/${puzzle.id}`);
@@ -19,6 +20,21 @@ const PuzzlePage = ({ images }) => {
     setSortBy(event.target.value);
     setSortOrder('asc');
   };
+
+  useEffect(() => {
+    const imagePromises = images.map((image) => {
+      return new Promise((resolve, reject) => {
+        const img = new Image();
+        img.src = image.thumbnail;
+        img.onload = resolve;
+        img.onerror = reject;
+      });
+    });
+
+    Promise.all(imagePromises)
+      .then(() => setAllImagesLoaded(true))
+      .catch(() => setAllImagesLoaded(true));
+  }, [images]);
 
   const sortedImages = images.slice().sort((a, b) => {
     if (sortBy === 'date') {
@@ -38,25 +54,31 @@ const PuzzlePage = ({ images }) => {
     <div className="page-wrapper">
       <MenuBar />
       <SortDropdown sortBy={sortBy} sortOrder={sortOrder} handleSortChange={handleSortChange} setSortOrder={setSortOrder} />
-      <div className="puzzle-flex-container">
-        <div className="puzzle-page">
-          {sortedImages.map((image, index) => (
-            <div key={index} className="puzzle-tile" onClick={() => redirectToPuzzleDetails(image)}>
-              <div className="puzzle-box">
-                <div className="puzzle-title">
-                  {image.isNew && <div className="new-puzzle">NEW</div>}
-                  {image.title}
+      {allImagesLoaded ? (
+        <div className="puzzle-flex-container">
+          <div className="puzzle-page">
+            {sortedImages.map((image, index) => (
+              <div key={index} className="puzzle-tile" onClick={() => redirectToPuzzleDetails(image)}>
+                <div className="puzzle-box">
+                  <div className="puzzle-title">
+                    {image.isNew && <div className="new-puzzle">NEW</div>}
+                    {image.title}
+                  </div>
+                  <div className="puzzle-details">
+                    <p className="puzzle-difficulty">{image.difficulty}</p>
+                    <p className="puzzle-date">{image.date}</p>
+                  </div>
+                  <img className="blurred-image" src={image.thumbnail} alt={`Puzzle ${index + 1}`} />
                 </div>
-                <div className="puzzle-details">
-                  <p className="puzzle-difficulty">{image.difficulty}</p>
-                  <p className="puzzle-date">{image.date}</p>
-                </div>
-                <img className="blurred-image" src={image.thumbnail} alt={`Puzzle ${index + 1}`} />
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
-      </div>
+      ) : (
+        <div className="loader-container">
+          <BarLoader color="#fff" loading={!allImagesLoaded} />
+        </div>
+      )}
       <div className="footer">
         <BottomBar />
       </div>
